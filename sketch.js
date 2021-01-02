7ae66c438f5c505bd425934c412b62b8537e7b4a
Shwetabh-coder <75842811+Shwetabh-coder@users.noreply.github.com>
2021-01-02 10:29:39 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,7 +4,6 @@ var speed,weight;
 
 function setup() {
   createCanvas(1600,400);
-  // createSprite(400, 200, 50, 50);
   speed = random(55,90);
   weight = (400,1500);
 
@@ -16,7 +15,6 @@ function setup() {
 
 function draw() {
   background(255,255,255);
-  // background(black);  
   if(wall.x-car.x < (car.width+wall.width)/2){
     car.velocityX = 0
     var deformation = 0.5*weight*speed*speed/22509;
@@ -33,13 +31,3 @@ function draw() {
   
   drawSprites();
 }
-
-// function isTouching(object1, object2) {
-//   if (object1.x - object2.x <= object2.width / 2 + object1.width / 2
-//     && object2.x - object1.x <= object2.width / 2 + object1.width / 2) {
-//          return true;
-//   }
-//   else {
-//          return false;
-//   }
-// }
